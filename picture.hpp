3638d7db8c4ad5b720a8749fa8b7d37be631dca8
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2021-03-27 17:39:00 +0500

    initialization

---
+++ picture.hpp
@@ -0,0 +1,4 @@
+void  house(); // 1 участник house.cpp
+void  sun(); // 2 участник sun.cpp
+void  man(); // 3 участник man.cpp
+void  fence(); // 4 участник fence.cpp
---
+++ program.cpp
@@ -0,0 +1,11 @@
+#include "graphics.h"
+#include "picture.hpp"
+int main()
+{ initwindow(800,600);
+  house();
+  sun();
+  man();
+  fence();
+  getch();
+  closegraph();
+}
